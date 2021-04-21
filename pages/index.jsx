63db4a4560c47287579c2c38b16cbeff562dc64f
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2021-04-21 17:48:28 +0100

    setup nextjs and scss

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+const Home = () => (
+  <>
+    <header>
+      <h2>Header</h2>
+    </header>
+    <main>
+      <h1>Hello!</h1>
+    </main>
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+  </>
+);
+
+export default Home;
