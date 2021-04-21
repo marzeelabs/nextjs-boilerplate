0a0cc6eea9e2a62f451d87d1f75455e9af0dc82d
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2021-04-21 17:58:15 +0100

    add eslint

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,5 @@
 import '../styles/main.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => <Component { ...pageProps } />;
 
-export default MyApp
+export default MyApp;
